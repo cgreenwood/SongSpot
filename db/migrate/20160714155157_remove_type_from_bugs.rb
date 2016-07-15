5e1f9d45a37dc5e7492b5aebc314ae6f01c3354c
Carl Greenwood <cgreenwood1994@gmail.com>
2016-07-15 10:27:37 +0100

    Removed unecessary files.

--- db/migrate/20160714155157_remove_type_from_bugs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromBugs < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :bugs, :type, :string
-  end
-end
--- db/migrate/20160715072420_add_bug_status_to_bugs.rb
+++ db/migrate/20160715072420_add_bug_status_to_bugs.rb
@@ -1,6 +1,5 @@
 class AddBugStatusToBugs < ActiveRecord::Migration[5.0]
   def change
-    remove_column :bugs, :bug_status, :string
     add_column :bugs, :bug_status, :string, default: 'Pending'
   end
 end
