c763a77af9df31928345e254487eff000c6372a3
Carl Greenwood <cgreenwood1994@gmail.com>
2016-07-15 10:41:22 +0100

    Modify tracks controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,6 @@
 # Comment
 class TracksController < ApplicationController
-  before_action all_tasks, only: [:add_favourite]
+  # before_action all_tasks, only: [:add_favourite]
   def add_favourite
   end
 end
